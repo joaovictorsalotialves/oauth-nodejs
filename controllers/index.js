e826112d086726f7fe3869c5b1672062959fa364
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-02 19:42:56 -0300

    feat: :sparkles: Controle de criação

--- controllers/index.js
+++ controllers/index.js
@@ -6,10 +6,15 @@ exports.showPageSignUp = (_req, res, _next) => {
   res.render('signUp')
 }
 
-exports.showMembersPage = (_req, res) => {
+exports.showMembersPage = (_req, res, _next) => {
   res.render('members')
 }
 
 exports.get404Page = (_req, res, _next) => {
   res.status(404).render('404')
+}
+
+exports.signup = (req, res, next) => {
+  const { username, email, password } = req.body
+  console.log(username, email, password)
 }
